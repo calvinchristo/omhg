526f36d556589ae4e0066920713cb60b1bef2278
calvinchristo <90774220+calvinchristo@users.noreply.github.com>
2021-09-28 18:47:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolor(){ 
+    document.getElementById("nameheading").style.color="red";
+                      }
+function changecolorwhite(){ 
+    document.getElementById("para").style.color="white"; 
+                      }
